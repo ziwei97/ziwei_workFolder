cbda125b12a30ccb556269bbc127d5d221c52054
ziweishi <s.ziwei@wustl.edu>
2023-06-05 15:50:43 -0500

    new comment

--- transfer_check.py
+++ transfer_check.py
@@ -55,11 +55,8 @@ def server_table_output(sql_path):
     return df,df2,site,check_path
 
 
-def guid_check(df,site,check_path):
-    db = download_whole_dynamodb_table.download_table("DFU_Master_ImageCollections")
-    db = db[db["StudyName"]=="DFU_SSP"]
+def guid_check(db,df,site,check_path):
     check_list = df["ImgCollGUID"].to_list()
-    print("total guid in "+site+" transfer is "+str(len(check_list)))
     db_list = db["ImgCollGUID"].to_list()
     index=0
     issue_guid=[]
@@ -82,21 +79,24 @@ def guid_check(df,site,check_path):
             index+=1
             status.append("new")
     if index==0:
-        print("all check!")
+        print(site+": "+"all check!")
     else:
-        print("total guid in "+site+" non-transfer is "+str(len(issue_guid)))
+        print(site+": "+str(len(issue_guid))+"/"+str(len(check_list)))
     data = zip(check_list,subject_id, status,folder)
     df_guid = pd.DataFrame(data, columns=["ImgCollGUID", "SubjectID","Transfer_Status","File_Location"])
-    status_file = os.path.join(check_path,"guid_log_out.xlsx")
+    file_name =site+"_guid_log_out.xlsx"
+    status_file = os.path.join(check_path,file_name)
     df_guid.to_excel(status_file)
-    return df_guid,db
+    return df_guid
 
 
-def image_check(df,df2,site,check_path):
-    df_guid,db = guid_check(df,site,check_path)
+def image_check(db,df,df2,site,check_path):
+    df_guid = guid_check(db,df,site,check_path)
     guid = df_guid["ImgCollGUID"].to_list()
     status={}
+    site_info=[]
     for i in guid:
+        site_info.append(site)
         comment={}
         df_sub = df2[df2["ImgCollGUID"]==i]
         db_sub = db[db["ImgCollGUID"] == i]
@@ -107,71 +107,100 @@ def image_check(df,df2,site,check_path):
             comment["Assessing Issue"] = "no guid"
             comment["Reference Issue"] = "no guid"
         else:
-            try:
-                raw_sub = df_sub[df_sub["ImageType"] == "Raw MSI"]
-                raw_list = raw_sub["ImageFileName"].to_list()
-                db_raw = db_sub["Raw"].iloc[0]
-                db_raw1 = db_raw.replace(" ", "")
-                db_raw1 = db_raw1.replace("Raw/Raw_", "")
-                db_raw1 = db_raw1.replace("'", "")
-                db_raw2 = db_raw1.strip("{}").split(",")
-                issue_list = []
-                for p in raw_list:
-                    if p not in db_raw2:
-                        issue_list.append(p)
-                if len(issue_list) > 0:
-                    comment["Raw Issue"] = issue_list
-                else:
-                    comment["Raw Issue"] = np.nan
-            except:
-                comment["Raw Issue"] = "no raw"
+            # check if pseudo in db
+            raw_sub = df_sub[df_sub["ImageType"] == "Raw MSI"]
+            raw_list = raw_sub["ImageFileName"].to_list()
+            if len(raw_list) > 0:
+                try:
+                    db_raw = db_sub["Raw"].iloc[0]
+                    db_raw1 = db_raw.replace(" ", "")
+                    db_raw1 = db_raw1.replace("Raw/Raw_", "")
+                    db_raw1 = db_raw1.replace("'", "")
+                    db_raw2 = db_raw1.strip("{}").split(",")
+                    issue_list = []
+
+                    for p in raw_list:
+                        if p not in db_raw2:
+                            issue_list.append(p)
+                    if len(issue_list) > 0:
+                        comment["Raw Issue"] = issue_list
+                    else:
+                        comment["Raw Issue"] = np.nan
+                except:
+                    comment["Raw Issue"] = "db no raw"
+
+            else:
+                comment["Raw Issue"] = "lc lost raw"
+
+
 
             # check if pseudo in db
-            try:
-                pseudo_sub = df_sub[df_sub["ImageType"] == "Pseudocolor"]
-                pseudo_list = pseudo_sub["ImageFileName"].to_list()
-                db_pseudo = db_sub["PseudoColor"].iloc[0]
-                db_pseudo1 = db_pseudo.replace("PseudoColor/", "")
-                db_pseudo1 = db_pseudo1.replace(" ", "")
-                db_pseudo1 = db_pseudo1.replace("'", "")
-                db_pseudo2 = db_pseudo1.strip("{}").split(",")
-                if len(pseudo_list) != len(db_pseudo2):
-                    comment["Pseudo Issue"] = "wrong pseudo"
-                else:
-                    comment["Pseudo Issue"] = np.nan
-            except:
-                comment["Pseudo Issue"] = "no pseudo"
+            pseudo_sub = df_sub[df_sub["ImageType"] == "Pseudocolor"]
+            pseudo_list = pseudo_sub["ImageFileName"].to_list()
+            if len(pseudo_list)>0:
+                try:
+                    db_pseudo = db_sub["PseudoColor"].iloc[0]
+                    db_pseudo1 = db_pseudo.replace("PseudoColor/", "")
+                    db_pseudo1 = db_pseudo1.replace(" ", "")
+                    db_pseudo1 = db_pseudo1.replace("'", "")
+                    db_pseudo2 = db_pseudo1.strip("{}").split(",")
+                    if len(pseudo_list) > 0:
+                        if len(pseudo_list) != len(db_pseudo2):
+                            comment["Pseudo Issue"] = "wrong pseudo"
+                        else:
+                            comment["Pseudo Issue"] = np.nan
+                except:
+                    comment["Pseudo Issue"] = "db no pseudo"
+            else:
+                comment["Pseudo Issue"] = "lc lost pseudo"
+
+
+
             # check if assessing in db
-            try:
-                assessing_sub = df_sub[df_sub["ImageType"] == "CJA"]
-                assessing_list = assessing_sub["ImageFileName"].to_list()
-                db_assessing = db_sub["Assessing"].iloc[0]
-                db_assessing1 = db_assessing.replace("Assessing/", "")
-                db_assessing1 = db_assessing1.replace(" ", "")
-                db_assessing1 = db_assessing1.replace("'", "")
-                db_assessing2 = db_assessing1.strip("{}").split(",")
-                if len(assessing_list) != len(db_assessing2):
-                    comment["Assessing Issue"] = "wrong assessing"
-                else:
-                    comment["Assessing Issue"] = np.nan
-            except:
-                comment["Assessing Issue"] = "no assessing"
+            assessing_sub = df_sub[df_sub["ImageType"] == "CJA"]
+            assessing_list = assessing_sub["ImageFileName"].to_list()
+            if len(assessing_list)>0:
+                try:
+                    db_assessing = db_sub["Assessing"].iloc[0]
+                    db_assessing1 = db_assessing.replace("Assessing/", "")
+                    db_assessing1 = db_assessing1.replace(" ", "")
+                    db_assessing1 = db_assessing1.replace("'", "")
+                    db_assessing2 = db_assessing1.strip("{}").split(",")
+                    if len(assessing_list) > 0:
+                        if len(assessing_list) != len(db_assessing2):
+                            comment["Assessing Issue"] = "wrong assessing"
+                        else:
+                            comment["Assessing Issue"] = np.nan
+                except:
+                    comment["Assessing Issue"] = "db no assessing"
+
+            else:
+                comment["Assessing Issue"] = "lc lost assessing"
+
+
+
 
             # check if reference in db
-            try:
-                reference_sub = df_sub[df_sub["ImageType"] == "Reference"]
-                reference_list = reference_sub["ImageFileName"].to_list()
-                db_reference = db_sub["Reference"].iloc[0]
-                db_reference1 = db_reference.replace("Reference/", "")
-                db_reference1 = db_reference1.replace(" ", "")
-                db_reference1 = db_reference1.replace("'", "")
-                db_reference2 = db_reference1.strip("{}").split(",")
-                if len(reference_list) != len(db_reference2):
-                    comment["Reference Issue"] = "wrong reference"
-                else:
-                    comment["Reference Issue"] = np.nan
-            except:
-                comment["Reference Issue"] = "no reference"
+            reference_sub = df_sub[df_sub["ImageType"] == "Reference"]
+            reference_list = reference_sub["ImageFileName"].to_list()
+            if len(reference_list)>0:
+                try:
+                    db_reference = db_sub["Reference"].iloc[0]
+                    db_reference1 = db_reference.replace("Reference/", "")
+                    db_reference1 = db_reference1.replace(" ", "")
+                    db_reference1 = db_reference1.replace("'", "")
+                    db_reference2 = db_reference1.strip("{}").split(",")
+                    if len(reference_list) > 0:
+                        if len(reference_list) != len(db_reference2):
+                            comment["Reference Issue"] = "wrong reference"
+                        else:
+                            comment["Reference Issue"] = np.nan
+                except:
+                    comment["Reference Issue"] = "db no reference"
+            else:
+                comment["Reference Issue"] = "lc lost reference"
+
+
         status[i] = comment
 
     raw_issue_list=[]
@@ -190,32 +219,41 @@ def image_check(df,df2,site,check_path):
     df_guid["Pseudo Issue"] = pseudo_issue_list
     df_guid["Assessing Issue"] = assessing_issue_list
     df_guid["Reference Issue"]=reference_issue_list
+    df_guid["Site"]=site_info
 
-
-    image_status_file = os.path.join(check_path, "image_log_out.xlsx")
+    file_name = site + "_image_log_out.xlsx"
+    image_status_file = os.path.join(check_path, file_name)
     df_guid.to_excel(image_status_file)
+    return df_guid
 
 
 
 
 if __name__ =="__main__":
+    db = download_whole_dynamodb_table.download_table("DFU_Master_ImageCollections")
+    db = db[db["StudyName"] == "DFU_SSP"]
     site_loc = {
         "nynw":"/Volumes/dfu/DataTransfers/nynw/DFU_SS/NYNW_DFU_SMD2223-008_04_11_23/SpectralView/dvsspdata.sql",
-        # "ocer":"/Volumes/dfu/DataTransfers/ocer/DFU_SS/OCER_DFU_SMD2148-019_04_25_23/SpectralView/dvsspdata.sql",
-        # "whfa":"/Volumes/dfu/DataTransfers/whfa/DFU_SS/WHFA_DFU_SMD2223-007_04_11_23/SpectralView/dvsspdata.sql",
-        # "youngst":"/Volumes/dfu/DataTransfers/youngst/DFU_SS/YOUNGST_DFU_SMD2223-010_03_20_23/SpectralView/dvsspdata.sql",
-        # "lvrpool":"/Volumes/dfu/DataTransfers/lvrpool/DFU_SS/LVRPOOL_DFU_SMD2223-009-03_20_23/SpectralView/dvsspdata.sql",
-        # "hilloh":"/Volumes/dfu/DataTransfers/hilloh/DFU_SS/HILLOH_DFU_SMD2225-011_04_25_23/SpectralView/dvsspdata.sql",
-        # "grovoh":"/Volumes/dfu/DataTransfers/grovoh/DFU_SS/GROVOH_DFU_SMD2225-013_04_25_23/SpectralView/dvsspdata.sql",
-        # "mentoh":"/Volumes/dfu/DataTransfers/mentoh/DFU_SS/MENTOH_DFU_SMD2223-007_06_01_23/SpectralView/dvsspdata.sql",
-        # "encinogho":"/Volumes/dfu/DataTransfers/encinogho/DFU_SS/ENCINO_DFU_SMD2225-018_05_15_23/SpectralView/dvsspdata.sql"
+        "ocer":"/Volumes/dfu/DataTransfers/ocer/DFU_SS/OCER_DFU_SMD2148-019_04_25_23/SpectralView/dvsspdata.sql",
+        "whfa":"/Volumes/dfu/DataTransfers/whfa/DFU_SS/WHFA_DFU_SMD2223-007_04_11_23/SpectralView/dvsspdata.sql",
+        "youngst":"/Volumes/dfu/DataTransfers/youngst/DFU_SS/YOUNGST_DFU_SMD2223-010_03_20_23/SpectralView/dvsspdata.sql",
+        "lvrpool":"/Volumes/dfu/DataTransfers/lvrpool/DFU_SS/LVRPOOL_DFU_SMD2223-009-03_20_23/SpectralView/dvsspdata.sql",
+        "hilloh":"/Volumes/dfu/DataTransfers/hilloh/DFU_SS/HILLOH_DFU_SMD2225-011_04_25_23/SpectralView/dvsspdata.sql",
+        "grovoh":"/Volumes/dfu/DataTransfers/grovoh/DFU_SS/GROVOH_DFU_SMD2225-013_04_25_23/SpectralView/dvsspdata.sql",
+        "mentoh":"/Volumes/dfu/DataTransfers/mentoh/DFU_SS/MENTOH_DFU_SMD2223-007_06_01_23/SpectralView/dvsspdata.sql",
+        "encinogho":"/Volumes/dfu/DataTransfers/encinogho/DFU_SS/ENCINO_DFU_SMD2225-018_05_15_23/SpectralView/dvsspdata.sql"
     }
     site_list=site_loc.keys()
+
+    data_sites=[]
     for i in site_list:
         path = site_loc[i]
         df_guid, df_image, site, check_path = server_table_output(path)
-        image_check(df_guid, df_image, site, check_path)
+        site_image= image_check(db,df_guid, df_image, site, check_path)
+        data_sites.append(site_image)
 
+    union_df = pd.concat(data_sites)
+    union_df.to_excel("/Users/ziweishi/Desktop/dfu_check.xlsx")
 
 
 
