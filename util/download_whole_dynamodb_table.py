7a20751a5d92e4cb75adbc7029db5ed0d794da53
shiziwei <zshi@spectralmd.com>
2023-09-10 14:51:27 -0500

    refactor database file and venv

--- util/download_whole_dynamodb_table.py
+++ util/download_whole_dynamodb_table.py
@@ -8,6 +8,8 @@ import numpy as np
 s3 = boto3.resource('s3')
 dynamodb = boto3.resource('dynamodb')
 
+path = "../Documents/Database/"
+
 def load_db(file_path: str) -> list:
     '''Loads the give json file with the data'''
     # Validate file existence
@@ -21,7 +23,7 @@ def load_db(file_path: str) -> list:
 
 def get_table(table_name, refresh=False):
 
-    file_path = os.path.join('/Users/ziweishi/Documents/database',f'{table_name}.pickle')
+    file_path = os.path.join(path,f'{table_name}.pickle')
 
     # if not pathlib.Path(file_path).exists() or refresh:
     #     # print(f'No database file for {table_name} found. Downloading file:')
@@ -33,14 +35,13 @@ def get_table(table_name, refresh=False):
 
 def update_db_file(table_name: str, debug: bool = True) -> list:
     '''Updates a file with the most current data from the DBs'''
-
     # User confirmation
     temp = input(f'Press "enter" to continue querying "{table_name}"')
     print("Starting...")
 
     # pathlib.Path(file_path).parent.mkdir(parents=True, exist_ok=True)
 
-    path_load = os.path.join('/Users/ziweishi/Documents/database',f'{table_name}.pickle')
+    path_load = os.path.join(path,f'{table_name}.pickle')
 
     try:
         dynamo_table = dynamodb.Table(table_name)
@@ -76,7 +77,7 @@ def update_db_file(table_name: str, debug: bool = True) -> list:
 
 def download_table(table_name):
     a = input("Refresh database yes or no: ")
-    excel_path = os.path.join("/Users/ziweishi/Documents/database",table_name+".xlsx")
+    excel_path = os.path.join(path,table_name+".xlsx")
     pickle_path = excel_path.replace(".xlsx",".pickle")
     if a =="yes":
         if os.path.isdir(excel_path) ==True:
@@ -101,7 +102,7 @@ if __name__ == "__main__":
 
     a = a[["ImgCollGUID","phase","StudyType","Bucket","Status","SubjectID","PseudoColor","Assessing","Raw","Mask","Tags"]]
 
-    a.to_excel("/Users/ziweishi/Desktop/dfuu.xlsx")
+    # a.to_excel("/Users/ziweishi/Desktop/dfuu.xlsx")
 
 
 
